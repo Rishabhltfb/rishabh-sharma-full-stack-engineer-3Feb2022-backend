cdf28dd980f7140b48b32bca8cedeb23b411906f
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2022-02-07 00:18:53 +0530

    feat: add restaurant filter query

---
+++ src/data/restaurant.data/restaurant.data.ts
@@ -0,0 +1,71 @@
+/* eslint-disable no-await-in-loop */
+import { Mongoose } from "mongoose";
+import Errors from "../../enums/errors";
+import GenericError from "../../models/dto/generic/generic-error";
+import RestaurantModel from "../../models/schema/restaurant.schema/restaurant.schema";
+import Restaurant from "../../models/types/restaurant.types/restaurant.type";
+
+const mongoose = new Mongoose();
+
+export default class RestaurantDAO {
+    async getAllRestaurants(): Promise<Array<Restaurant>> {
+        try {
+            const restaurants: Array<Restaurant> = await RestaurantModel.find();
+            if (!restaurants) {
+                throw new GenericError(Errors.RESTAURANT_NOT_FOUND_ERR, 404);
+            }
+            return restaurants;
+        } catch (err) {
+            if (err instanceof mongoose.Error.CastError) {
+                throw new GenericError(Errors.RESTAURANT_NOT_FOUND_ERR, 404);
+            } else {
+                throw err;
+            }
+        }
+    }
+
+    async getRestaurantByName(name: string): Promise<Array<Restaurant>> {
+        try {
+            var nameRegex = new RegExp(String(name));
+
+            const restaurants: Array<Restaurant> = await RestaurantModel.find({
+                restaurantName: { $regex: nameRegex },
+            });
+            if (!restaurants) {
+                throw new GenericError(Errors.RESTAURANT_NOT_FOUND_ERR, 404);
+            }
+            return restaurants;
+        } catch (err) {
+            if (err instanceof mongoose.Error.CastError) {
+                throw new GenericError(Errors.RESTAURANT_NOT_FOUND_ERR, 404);
+            } else {
+                throw err;
+            }
+        }
+    }
+
+    async getRestaurantsByDayTime(
+        day: string,
+        openingTime: number,
+        closingTime: number
+    ): Promise<Array<Restaurant>> {
+        try {
+            const restaurants: Array<Restaurant> = await RestaurantModel.find()
+                .where(`schedule.${day}.openingTime`)
+                .lte(openingTime)
+                .where(`schedule.${day}.closingTime`)
+                .gte(closingTime)
+                .exec();
+            if (!restaurants) {
+                throw new GenericError(Errors.RESTAURANT_NOT_FOUND_ERR, 404);
+            }
+            return restaurants;
+        } catch (err) {
+            if (err instanceof mongoose.Error.CastError) {
+                throw new GenericError(Errors.RESTAURANT_NOT_FOUND_ERR, 404);
+            } else {
+                throw err;
+            }
+        }
+    }
+}
--- src/enums/errors.ts
+++ src/enums/errors.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 enum Errors {
     NOT_FOUND_ERR = "Not Found",
+    RESTAURANT_NOT_FOUND_ERR = "Restaurant Not found",
     SERVER_ERR = "Internal Server Error! Something went wrong",
-
     UNAUTHORIZED_ERR = "Unauthorized",
-
     INCORRECT_PASSWORD = "Password incorrect.",
+    INVALID_TIME_INPUT_ERROR = "Invalid openingTime and closingTime",
 }
 export default Errors;
